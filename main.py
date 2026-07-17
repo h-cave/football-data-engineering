92068998d85e47b7f1ae382f58ec5f1eaf0907c3
h-cave <124801533+h-cave@users.noreply.github.com>
2026-07-17 18:30:11 +0100

    Getting total passes and total in and out of possesion

--- main.py
+++ main.py
@@ -2,24 +2,60 @@ import json
 
 open_data_path = "/home/harry/projects/open-data/data"
 
+def which_team_passed(data):
+    if data['possession_team']['name'] == "Everton":
+            return 'Everton'
+    else:
+        return 'Arsenal'
+
 with open(f"{open_data_path}/events/3749493.json", "r") as file:
     data = json.load(file)
 
-print(data)
-print("Data type")
-print(type(data))
-
 records = 0
+passes = 0
+passes_in_open_play = 0
+total_everton_passes = 0
+total_arsenal_passes = 0
+open_play_passes_everton = 0
+open_play_passes_arsenal = 0
 for data in data:
     records += 1
-    print(f"Record {records}: {data['type']['name']}")
+    print(f"data: {data}")
+    # print(f"Record {records}: {data['type']['name']}")
+    if data['type']['name'] == "Pass":
+        passes += 1
+        team = which_team_passed(data)
+        if data['play_pattern']['name'] in ["Regular Play", "From Keeper"]:
+            passes_in_open_play += 1
+            if team == 'Everton':
+                open_play_passes_everton += 1
+            else:
+                open_play_passes_arsenal += 1
+        else:
+            if team == 'Everton':
+                total_everton_passes += 1
+            else:
+                total_arsenal_passes += 1
+        
+
+
+
+
 
 
 print ("#########RECORDS#########")
 print(f"Total Records: {records}")
-
+print(f"Total Passes: {passes}")
+print(f"Everton Passes: {total_everton_passes}")
+print(f"Arsenal Passes: {total_arsenal_passes}")
+print(f"Passes in Open Play: {passes_in_open_play}")
+print(f"Open Play Passes Everton: {open_play_passes_everton}")
+print(f"Open Play Passes Arsenal: {open_play_passes_arsenal}")
 print ("#########data#########")
 print(f"LENGTH:{len(data)}")
+print(data)
+print("Data type")
+print(type(data))
 
 
 res = next(iter(data))
